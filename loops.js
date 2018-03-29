63998219d857394f945c20c2a7e8a39a47af77a1
piotrkulak@outlook.com <piotrkulak@outlook.com>
2018-03-29 21:50:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,30 @@
-for (let i = 0; i<1; i++) {
-  console.log("I am {i=1} strange loop")}
-  
+function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-  console.log("I am ${i} strange loops")}
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } 
+    else 
+    {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+  return 'done'
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+  do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
+  return array
+}
